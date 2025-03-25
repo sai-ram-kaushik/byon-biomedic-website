df9902aa031b5258c9c64255f87b51604a38a204
Aditya Singh <66196917+singhaditya28@users.noreply.github.com>
2025-03-25 09:51:38 +0530

    footer navy blue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
    return (
-      <div className="w-full mt-10 p-6 px-5 lg:px-10 bg-gray-900 text-gray-300">
+      <div className="w-full mt-10 p-6 px-5 lg:px-10 bg-[#182A40] text-white">
          <div className="container mx-auto">
             <div className="grid grid-cols-1 lg:grid-cols-4 items-center gap-5 lg:gap-16">
                <div className="flex flex-col items-start gap-3">
@@ -16,35 +16,27 @@ const Footer = () => {
                </div>
 
                <div className="flex flex-col items-start gap-5">
-                  <p className="font-bold text-white">Quick Links</p>
+                  <p className="font-bold">Quick Links</p>
                   <div className="flex flex-col items-start gap-2">
-                     <Link to="/">
-                        <p className="hover:text-primary cursor-pointer">Home</p>
-                     </Link>
-                     <Link to="/about">
-                        <p className="hover:text-primary cursor-pointer">About Us</p>
-                     </Link>
-                     <Link to="/medicines">
-                        <p className="hover:text-primary cursor-pointer">Medicines</p>
-                     </Link>
+                     <Link to="/"><p className="hover:text-gray-300 cursor-pointer">Home</p></Link>
+                     <Link to="/about"><p className="hover:text-gray-300 cursor-pointer">About Us</p></Link>
+                     <Link to="/medicines"><p className="hover:text-gray-300 cursor-pointer">Medicines</p></Link>
                   </div>
                </div>
 
                <div className="flex flex-col items-start gap-5">
-                  <p className="font-bold text-white">Contact Us</p>
+                  <p className="font-bold">Contact Us</p>
                   <div className="flex flex-col items-start gap-2">
                      <div className="flex items-center gap-2">
                         <IoMdMail size={15} className="text-primary" />
                         <a href="mailto:byonbiomedic@gmail.com">
-                           <p>info@byonbiomedic.com</p>
+                           <p className="hover:text-gray-300">info@byonbiomedic.com</p>
                         </a>
                      </div>
 
                      <div className="flex items-start gap-2">
                         <FaPhoneAlt size={15} className="text-primary" />
-                        <div className="flex flex-col items-start">
-                           <p>+603 62422746</p>
-                        </div>
+                        <p>+603 62422746</p>
                      </div>
 
                      <div className="flex items-start gap-2">
@@ -58,16 +50,16 @@ const Footer = () => {
                </div>
 
                <div className="flex flex-col items-start gap-5">
-                  <p className="font-bold text-white">Privacy</p>
+                  <p className="font-bold">Privacy</p>
                   <div className="flex flex-col items-start gap-2">
-                     <p>FAQs</p>
-                     <Link to={`/terms-and-conditions`}><p>Terms</p></Link>
-                     <Link to={`/privacy-policy`}> <p>Privacy</p></Link>
+                     <p className="hover:text-gray-300">FAQs</p>
+                     <Link to={`/terms-and-conditions`}><p className="hover:text-gray-300">Terms</p></Link>
+                     <Link to={`/privacy-policy`}><p className="hover:text-gray-300">Privacy</p></Link>
                   </div>
                </div>
             </div>
 
-            <hr className="mt-5 border-gray-700" />
+            <hr className="mt-5 border-gray-500" />
 
             <div className="flex flex-col lg:flex-row gap-3 items-center justify-between py-3">
                <p>2025 - All Rights Reserved</p>
