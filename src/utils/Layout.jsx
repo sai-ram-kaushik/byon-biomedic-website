53a1fd281baa513f38aabda8c8e0a15047df8b12
Aditya Singh <66196917+singhaditya28@users.noreply.github.com>
2025-04-07 15:30:33 +0530

    layout

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import data from '../data/landing-page.json'
+import data from '../data/landing-page.json';
 
 const Layout = ({ children }) => {
    return (
       <div className="flex flex-col min-h-screen">
-         <Navbar navbar={data.navbar}/>
-         <main className="flex-grow">{children}</main>
+         <Navbar navbar={data.navbar} />
+         
+         {/* Spacer div to offset fixed navbar (80px = h-20 = pt-20) */}
+         <div className="pt-20 flex-grow">
+            {children}
+         </div>
+
          <Footer />
       </div>
    );
